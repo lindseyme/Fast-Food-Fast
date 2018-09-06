a2df8eda2ac4532d148acd5a90bbc3f5ad3ab2c6
Ayesiga Patra Lindsey <ayesigalindsey@gmail.com>
2018-09-06 12:39:56 -0700

    Make order module finished [delivers #160279744]

---
+++ js/makeOrderModal.js
@@ -0,0 +1,193 @@
+function order1(){
+                    // Get modal element
+                var modal = document.getElementById('simpleModal');
+                // Get open modal button
+                var modalBtn = document.getElementById('modalBtn1');
+                // Get close button
+                var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+                // Listen for open click
+                modalBtn.addEventListener('click', openModal);
+                // Listen for close click
+                closeBtn.addEventListener('click', closeModal);
+                // Listen for outside click
+                window.addEventListener('click', outsideClick);
+
+                // Function to open modal
+                function openModal(){
+                modal.style.display = 'block';
+                }
+
+                // Function to close modal
+                function closeModal(){
+                modal.style.display = 'none';
+                }
+
+                // Function to close modal if outside click
+                function outsideClick(e){
+                if(e.target == modal){
+                    modal.style.display = 'none';
+                }
+                }
+              }
+              function order2(){
+                    // Get modal element
+                var modal = document.getElementById('simpleModal');
+                // Get open modal button
+                var modalBtn = document.getElementById('modalBtn2');
+                // Get close button
+                var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+                // Listen for open click
+                modalBtn.addEventListener('click', openModal);
+                // Listen for close click
+                closeBtn.addEventListener('click', closeModal);
+                // Listen for outside click
+                window.addEventListener('click', outsideClick);
+
+                // Function to open modal
+                function openModal(){
+                modal.style.display = 'block';
+                }
+
+                // Function to close modal
+                function closeModal(){
+                modal.style.display = 'none';
+                }
+
+                // Function to close modal if outside click
+                function outsideClick(e){
+                if(e.target == modal){
+                    modal.style.display = 'none';
+                }
+                }
+              }
+
+               function order3(){
+                    // Get modal element
+                var modal = document.getElementById('simpleModal');
+                // Get open modal button
+                var modalBtn = document.getElementById('modalBtn3');
+                // Get close button
+                var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+                // Listen for open click
+                modalBtn.addEventListener('click', openModal);
+                // Listen for close click
+                closeBtn.addEventListener('click', closeModal);
+                // Listen for outside click
+                window.addEventListener('click', outsideClick);
+
+                // Function to open modal
+                function openModal(){
+                modal.style.display = 'block';
+                }
+
+                // Function to close modal
+                function closeModal(){
+                modal.style.display = 'none';
+                }
+
+                // Function to close modal if outside click
+                function outsideClick(e){
+                if(e.target == modal){
+                    modal.style.display = 'none';
+                }
+                }
+              }
+               function order4(){
+                    // Get modal element
+                var modal = document.getElementById('simpleModal');
+                // Get open modal button
+                var modalBtn = document.getElementById('modalBtn4');
+                // Get close button
+                var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+                // Listen for open click
+                modalBtn.addEventListener('click', openModal);
+                // Listen for close click
+                closeBtn.addEventListener('click', closeModal);
+                // Listen for outside click
+                window.addEventListener('click', outsideClick);
+
+                // Function to open modal
+                function openModal(){
+                modal.style.display = 'block';
+                }
+
+                // Function to close modal
+                function closeModal(){
+                modal.style.display = 'none';
+                }
+
+                // Function to close modal if outside click
+                function outsideClick(e){
+                if(e.target == modal){
+                    modal.style.display = 'none';
+                }
+                }
+              }
+               function order5(){
+                    // Get modal element
+                var modal = document.getElementById('simpleModal');
+                // Get open modal button
+                var modalBtn = document.getElementById('modalBtn5');
+                // Get close button
+                var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+                // Listen for open click
+                modalBtn.addEventListener('click', openModal);
+                // Listen for close click
+                closeBtn.addEventListener('click', closeModal);
+                // Listen for outside click
+                window.addEventListener('click', outsideClick);
+
+                // Function to open modal
+                function openModal(){
+                modal.style.display = 'block';
+                }
+
+                // Function to close modal
+                function closeModal(){
+                modal.style.display = 'none';
+                }
+
+                // Function to close modal if outside click
+                function outsideClick(e){
+                if(e.target == modal){
+                    modal.style.display = 'none';
+                }
+                }
+              }
+               function order6(){
+                    // Get modal element
+                var modal = document.getElementById('simpleModal');
+                // Get open modal button
+                var modalBtn = document.getElementById('modalBtn6');
+                // Get close button
+                var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+                // Listen for open click
+                modalBtn.addEventListener('click', openModal);
+                // Listen for close click
+                closeBtn.addEventListener('click', closeModal);
+                // Listen for outside click
+                window.addEventListener('click', outsideClick);
+
+                // Function to open modal
+                function openModal(){
+                modal.style.display = 'block';
+                }
+
+                // Function to close modal
+                function closeModal(){
+                modal.style.display = 'none';
+                }
+
+                // Function to close modal if outside click
+                function outsideClick(e){
+                if(e.target == modal){
+                    modal.style.display = 'none';
+                }
+                }
+              }
