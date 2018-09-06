8fbd630f02fe3c704fbc052c09baa3c9f6f6efe1
Ayesiga Patra Lindsey <ayesigalindsey@gmail.com>
2018-09-06 15:02:30 -0700

    Manage users' order module finished. [delivers #160279464]

--- js/admin_viewOrder.js
+++ js/admin_viewOrder.js
@@ -1,9 +1,9 @@
-    function addFoodItem(){
-        // Get modal element
-                var modal = document.getElementById('addFoodModal');
+function order1(){
+             
+                // Get modal element
+                var modal = document.getElementById('simpleModal');
                 // Get open modal button
-                var modalBtn = document.getElementById('addFood');
-
+                var modalBtn = document.getElementById('viewOrder');
                 // Get close button
                 var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
@@ -30,13 +30,14 @@
                     modal.style.display = 'none';
                 }
                 }
-    }
-    function editModal1(){
-        // Get modal element
-                var modal = document.getElementById('editModal');
+            }
+           
+           function order2(){
+             
+                // Get modal element
+                var modal = document.getElementById('simpleModal');
                 // Get open modal button
-                var modalBtn = document.getElementById('edit1');
-
+                var modalBtn = document.getElementById('viewOrder1');
                 // Get close button
                 var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
@@ -63,38 +64,4 @@
                     modal.style.display = 'none';
                 }
                 }
-    }
-    function editModal2(){
-        // Get modal element
-                var modal = document.getElementById('editModal');
-                // Get open modal button
-                var modalBtn = document.getElementById('edit2');
-
-                // Get close button
-                var closeBtn = document.getElementsByClassName('closeBtn')[0];
-
-                // Listen for open click
-                modalBtn.addEventListener('click', openModal);
-                // Listen for close click
-                closeBtn.addEventListener('click', closeModal);
-                // Listen for outside click
-                window.addEventListener('click', outsideClick);
-
-                // Function to open modal
-                function openModal(){
-                modal.style.display = 'block';
-                }
-
-                // Function to close modal
-                function closeModal(){
-                modal.style.display = 'none';
-                }
-
-                // Function to close modal if outside click
-                function outsideClick(e){
-                if(e.target == modal){
-                    modal.style.display = 'none';
-                }
-                }
-    }
-            
+            }
