0d0db3ff5f6f195add020323b7a0b04267e0dd64
Ayesiga Patra Lindsey <ayesigalindsey@gmail.com>
2018-09-06 17:04:02 -0700

    The view order history task finished, and all files have been linked. [delivers #160280141]

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Fast-Food-Fast
+
+#admin login credentials
+email:admin@admin.com
+password:admin1234
+
+#user login credentials
+email:user@user.com
+password:user1234
--- js/login.js
+++ js/login.js
@@ -8,3 +8,20 @@ function swap(referTo){//referTo refers to the current div where this function i
                         referTo.parentNode.classList.add("signup");//adds the signup form
                     }
                 }
+function getCredentials(){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+        if(email == "admin@admin.com" && password=="admin1234"){
+           window.location= "manageFastFood.html";
+           alert(window.location);
+        }
+        else if(email == "user@user.com" && password=="user1234"){
+            window.location= "menu.html";
+           alert(window.location);
+        }
+        else{
+        alert("Wrong credentials");
+         }
+    }
+    
