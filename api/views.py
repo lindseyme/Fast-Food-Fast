a7b25f878dd9038b35f5adcfe267ab7b752def1c
Ayesiga Patra <ayesigalindsey@gmail.com>
2018-09-19 08:34:11 -0700

    Update views.py

--- api/views.py
+++ api/views.py
@@ -44,8 +44,8 @@ class Order(MethodView):
                                       request.json['username'], request.json['order_list'], None)
                 self.orders.append(new_order)
                 return jsonify({'new_order': new_order.__dict__}), 200
-            raise ValueError("order_list should be a str")
-        raise ValueError("order_list and username keys should be defined in json structure")
+            return jsonify({"error message":"order_list should be a str"})
+        return jsonify({"error message":"Define order_list and username keys in json structure"})
 
     def put(self, order_id):
         """
@@ -54,20 +54,16 @@ class Order(MethodView):
         if isinstance(order_id, int):
             if not isinstance(order_id, bool):
                 if order_id > 0:
-                   # return 200
-                    fetch_order = [order for order in self.orders
-                                   if order.__dict__['order_id'] == order_id]
-                    if fetch_order:
-                        if 'order_status' in request.json and isinstance(request.json
-                                                                         ['order_status'], str):
-                            for order in self.orders:
-                                if order.__dict__['order_id'] == order_id:
-                                    order_json = request.get_json()
-                                    order.__dict__['order_status'] = order_json['order_status']
-                                    return jsonify({'updated order':
-                                                    [order.__dict__ for order in self.orders]})
-                        return "A string order_status should be defined"
-                    return jsonify({"Error Message":"The order with that id doesn't exist."})
-                raise ValueError("order_id should be greater than 0")
+                    if 'order_status' in request.json and isinstance(request.json
+                                                                    ['order_status'], str):
+                        for order in self.orders:
+                            if order.__dict__['order_id'] == order_id:
+                                order_json = request.get_json()
+                                order.__dict__['order_status'] = order_json['order_status']
+                                return jsonify({'updated order':
+                                                [order.__dict__ for order in self.orders]})
+                        return jsonify({"Error Message":"The order with that id doesn't exist."})
+                    return jsonify({"error message":"A string order_status should be defined"})
+                raise ValueError("Please provide a non negative integer argument")   
             raise TypeError("Please provide a non negative integer argument")
         raise TypeError("Please provide a non negative integer argument")
