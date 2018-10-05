a20dfa16f47511c2a24d5992c3c1f7f9e99e47c2
Ayesiga Patra <ayesigalindsey@gmail.com>
2018-10-05 09:46:02 -0700

    [chore] Add the app docs

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,9 @@ conn = psycopg2.connect(database = "testdb")
 # Import the application views
 from app.views.user_views import GetAuthUrls
 from app.views.menu_views import GetMenuUrls
+from app.docs.views import docs
 from app.views.order_views import GetOrderUrls
 GetAuthUrls.fetch_urls(app)
 GetMenuUrls.fetch_urls(app)
 GetOrderUrls.fetch_urls(app)
+app.register_blueprint(docs)
