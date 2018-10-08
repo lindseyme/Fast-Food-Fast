26d86092c08039b8c9ff362957b5e1e310b97ac4
Ayesiga Patra <ayesigalindsey@gmail.com>
2018-10-08 10:02:18 -0700

    [chore] improve database settings

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,10 @@ app.config.from_object(app_settings)
 bcrypt = Bcrypt(app)
 
 # Initialize connections to  postgresql server
-conn = psycopg2.connect(database = "testdb",user="postgres",password="")
+if os.getenv('db') == 'heroku':
+    conn = psycopg2.connect(database = "d3jpf1e4bm92ff",user="jxboodptewpeaq",password="d5c8d9c2ab8aeff7fdb73701c1cacd3803d39f7c1320c21241824e85e3254091" host="ec2-174-129-32-37.compute-1.amazonaws.com",port=5432)
+else:
+    conn = psycopg2.connect(database = "testdb")
 
 # Import the application views
 from app.views.user_views import GetAuthUrls
