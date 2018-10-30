332af4b44b6ef9952ace88e184b8b7d5719ededa
Ayesiga Patra <ayesigalindsey@gmail.com>
2018-10-30 12:47:45 -0700

    [chore] modify api documentation

--- app/__init__.py
+++ app/__init__.py
@@ -26,12 +26,15 @@ bcrypt = Bcrypt(app)
 if os.getenv('db') == 'heroku':
     conn = psycopg2.connect(database = "d3jpf1e4bm92ff",user="jxboodptewpeaq",password="d5c8d9c2ab8aeff7fdb73701c1cacd3803d39f7c1320c21241824e85e3254091", host="ec2-174-129-32-37.compute-1.amazonaws.com",port=5432)
 else:
-    conn = psycopg2.connect(database = "testdb", user="postgres", password="admin", host="localhost",port=5555)
+    conn = psycopg2.connect(database = "testdb")
 
 # Import the application views
 from app.views.user_views import GetAuthUrls
 from app.views.menu_views import GetMenuUrls
 from app.views.order_views import GetOrderUrls
+from app.docs.views import docs
+
+app.register_blueprint(docs)
 GetAuthUrls.fetch_urls(app)
 GetMenuUrls.fetch_urls(app)
 GetOrderUrls.fetch_urls(app)
